a27f0c6f7efd9f0dc83f8cac67cfe3d7b199ebf1
BigMorris <bigmaumo@gmail.com>
2021-12-03 14:58:15 +0200

    Maurice

--- app/src/main/java/com/example/speakout/session/UserSession.kt
+++ app/src/main/java/com/example/speakout/session/UserSession.kt
@@ -2,7 +2,8 @@ package com.example.speakout.session
 
 class UserSession
 {
-    companion object{
+    companion object
+    {
         var user_id:String?=null;
         var user_category:String?=null;
         var user_gender:String?=null;
